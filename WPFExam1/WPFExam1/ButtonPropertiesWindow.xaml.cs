73c5d97cf6e67f460aa90f357f528ef77564a2d8
layanmeta <66564396+layanmeta@users.noreply.github.com>
2021-05-04 14:03:53 +0300

    Add files via upload

---
+++ WPFExam1/WPFExam1/ButtonPropertiesWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFExam1
+{
+    /// <summary>
+    /// Interaction logic for ButtonPropertiesWindow.xaml
+    /// </summary>
+    public partial class ButtonPropertiesWindow : Window
+    {
+        public ButtonPropertiesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFExam1/WPFExam1/somethingblabla.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFExam1
+{
+    class somethingblabla : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null) return "ButtonSize";
+
+            var sliderObject = (Slider)parameter;
+            double minimum = sliderObject.Minimum;
+            double maximum = sliderObject.Maximum;            
+
+            var range = maximum - minimum;
+            double fixedValue = (double)value - minimum;
+
+            var percentage = (fixedValue * 100) / range;
+
+            if (percentage <=  25)
+            {
+                return "small";
+            }
+            else if (percentage > 25 && percentage <= 50)
+            {
+                return "Medium";
+            }
+            else if (percentage > 50 && percentage <=75)
+            {
+                return "large";
+            }
+            return "Extra large";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
